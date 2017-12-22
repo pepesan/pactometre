e49190f9d56643ac78a09db0cd99af6b6a233bc5
pepesan <pepesan@gmail.com>
2017-12-22 10:30:44 +0100

    subida inicial, datso preparados y prototipo de html

---
+++ js/datos.js
@@ -0,0 +1,60 @@
+var ciudadanos={
+    nombre:"Ciudadanos",
+    diputados:37,
+    color:"orange"
+};
+var resultados=[ciudadanos];
+var juntspercat={
+    nombre:"Junts per Catalunya",
+    diputados:34,
+    color:"pink"
+};
+resultados=[ciudadanos,juntspercat];
+var esquerra={
+    nombre:"Esquerra Republicana de Catalunya",
+    diputados:32,
+    color:"#FDB94D"
+};
+resultados=[ciudadanos,juntspercat,esquerra];
+var psc={
+    nombre:"Partit de Socialistes de Catalunya",
+    diputados:17,
+    color:"red"
+};
+resultados=[ciudadanos,juntspercat,esquerra,psc];
+var comunes={
+    nombre:"Catalunya en Comú - Podem",
+    diputados:8,
+    color:"purple"
+};
+resultados=[ciudadanos,juntspercat,esquerra,psc,comunes];
+var cup={
+    nombre:"CUP",
+    diputados:4,
+    color:"yellow"
+};
+resultados=[ciudadanos,juntspercat,esquerra,psc, comunes, cup];
+var pp={
+    nombre:"Partit Popular de Catalunya",
+    diputados:3,
+    color:"cyan"
+};
+resultados=[ciudadanos,juntspercat,esquerra,psc, comunes, cup, pp];
+
+
+/*
+[
+    {
+        "nombre":"Ciudadanos",
+        "diputados":37,
+        "color":"orange"
+    },
+    {
+    nombre:"Junts per Catalunya",
+    diputados:34,
+    color:"pink"
+    },
+    ....
+]
+
+*/
