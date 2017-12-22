8dad5f5e5fb9c6c9adba1fe0868ff3d31948e8aa
pepesan <pepesan@gmail.com>
2017-12-22 13:29:28 +0100

    colocada funcion e interactividad por parte del usuario

--- js/datos.js
+++ js/datos.js
@@ -1,14 +1,22 @@
+/*
+Obtención de datos
+*/
+
+//creacion de un objeto
 var ciudadanos={
     nombre:"Ciudadanos",
     diputados:37,
     color:"orange"
 };
+//Declaración de un array con un elemento
 var resultados=[ciudadanos];
+//Declaración de otro objeto
 var juntspercat={
     nombre:"Junts per Catalunya",
     diputados:34,
     color:"pink"
 };
+//Asignación de un array de dos elementos
 resultados=[ciudadanos,juntspercat];
 var esquerra={
     nombre:"Esquerra Republicana de Catalunya",
@@ -16,6 +24,7 @@ var esquerra={
     color:"#FDB94D"
 };
 resultados=[ciudadanos,juntspercat,esquerra];
+
 var psc={
     nombre:"Partit de Socialistes de Catalunya",
     diputados:17,
@@ -41,15 +50,42 @@ var pp={
 };
 resultados=[ciudadanos,juntspercat,esquerra,psc, comunes, cup, pp];
 
-//document.getElementById("listado").innerHTML="<li>Ciudadanos: 37</li>";
-document.getElementById("listado").innerHTML="";
-document.getElementById("listado").innerHTML+="<li>"+resultados[0].nombre+": "+resultados[0].diputados+"</li>";
-document.getElementById("listado").innerHTML+="<li>"+resultados[1].nombre+": "+resultados[1].diputados+"</li>";
-document.getElementById("listado").innerHTML+="<li>"+resultados[2].nombre+": "+resultados[2].diputados+"</li>";
-document.getElementById("listado").innerHTML+="<li>"+resultados[3].nombre+": "+resultados[3].diputados+"</li>";
-document.getElementById("listado").innerHTML+="<li>"+resultados[4].nombre+": "+resultados[4].diputados+"</li>";
-document.getElementById("listado").innerHTML+="<li>"+resultados[5].nombre+": "+resultados[5].diputados+"</li>";
-document.getElementById("listado").innerHTML+="<li>"+resultados[6].nombre+": "+resultados[6].diputados+"</li>";
+
+ function imprimeDatos(){   
+    /*
+    Buscamos en la estructura el elemento a modificar
+    */
+    //limpiamos el contenido (Poda)
+     document.getElementById("listado").innerHTML="";
+
+    //Añadimos los nuevos elementos (Injerto)
+
+    /*
+    document.getElementById("listado").innerHTML
+        +=
+    //"<li>(nombre): (diputados)</li>"
+    "<li>"+resultados[0].nombre+": "+resultados[0].diputados+"</li>";
+    document.getElementById("listado").innerHTML+="<li>"+resultados[1].nombre+": "+resultados[1].diputados+"</li>";
+    document.getElementById("listado").innerHTML+="<li>"+resultados[2].nombre+": "+resultados[2].diputados+"</li>";
+    document.getElementById("listado").innerHTML+="<li>"+resultados[3].nombre+": "+resultados[3].diputados+"</li>";
+    document.getElementById("listado").innerHTML+="<li>"+resultados[4].nombre+": "+resultados[4].diputados+"</li>";
+    document.getElementById("listado").innerHTML+="<li>"+resultados[5].nombre+": "+resultados[5].diputados+"</li>";
+    document.getElementById("listado").innerHTML+="<li>"+resultados[6].nombre+": "+resultados[6].diputados+"</li>";
+
+    document.getElementById("listado").innerHTML+="<li>"+resultados[?].nombre+": "+resultados[?].diputados+"</li>";
+    */
+    for(indice in resultados){
+        document.getElementById("listado").innerHTML+="<li>"+resultados[indice].nombre+": "+resultados[indice].diputados+"</li>";
+    }
+    // console.log();
+}
+
+
+//imprimeDatos();
+//Cambiar el comportamiento del botón para que cuando lo pulsen ejecute una función que yo le diga
+document.getElementById("miboton")
+    .addEventListener("click",imprimeDatos);
+
 
 /*
 [
